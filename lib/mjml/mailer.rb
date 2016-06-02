09e062869a2d7e11868902c78a60c0bf84bc30d2
MQuy <sugiacupit@gmail.com>
2016-06-02 17:05:33 +0800

    Support mjml mailer

--- lib/mjml/mailer.rb
+++ lib/mjml/mailer.rb
@@ -1,13 +1,40 @@
 module Mjml
   module Mailer
-    def mail(headers, &block)
+    def mjml(headers, &block)
       scope = Mjml::Scope.new(self, headers[:template_variables])
-      content = _layout ? mjml_layout_content(headers, scope) : mjml_template_content(headers, scope)
 
-      headers[:body] = Mjml.template(content)
+      headers[:subject] ||= I18n.t("mailers.#{mailer_name}.#{action_name}.subject")
+      headers[:template_name] ||= action_name
+      headers[:template_path] ||= "#{Dir.pwd}/app/views/mailers/#{mailer_name}"
+      headers[:body] = mjml_content(headers, scope)
       headers[:content_type] = "text/html"
 
-      super(headers, &block)
+      mail(headers, &block)
+    end
+
+    def mailer_name
+      self.class.name.underscore.sub(/_mailer$/, '')
+    end
+
+    def t(pharse, args = {})
+      if pharse =~ /\A\./
+        I18n.t("mailers.#{mailer_name}.#{action_name}#{pharse}", args)
+      else
+        super
+      end
+    end
+
+    private
+
+    def mjml_content(headers, scope)
+      content =
+        if _layout
+          mjml_layout_content(headers, scope)
+        else
+          mjml_template_content(headers, scope)
+        end
+
+      Mjml.template(content)
     end
 
     def mjml_layout_content(headers, scope)
